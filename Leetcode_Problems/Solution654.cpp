345d17fe882dc357a3c92641f8c01e963277fb19
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-03 20:31:29 +0530

    LeetCode Problems Solution

---
+++ Leetcode_Problems/Solution654.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Node class for the binary tree
+class TreeNode {
+    public:
+
+    // Data Members
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        this->val = x;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+// Class Solution
+class Solution {
+    public:
+    
+    int index = -1;
+    
+    // Build Binary Tree
+    TreeNode* buildTree(vector<int>& nums) {
+        index++;
+        if (index >= nums.size() || nums[index] == -1) {
+            return nullptr;
+        }
+
+        // Create a new node with the current value
+        TreeNode* node = new TreeNode(nums[index]);
+
+        // Recursively build the left and right subtrees
+        node->left = buildTree(nums);
+        node->right = buildTree(nums);
+
+        return node;
+    }
+
+    // Function to construct the maximum binary tree
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return buildTree(nums, 0, nums.size() - 1);
+    }
+
+    // Helper function to build the tree recursively
+    TreeNode* buildTree(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+
+        // Find the index of the maximum element in the current range
+        int maxIndex = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        // Create a new node with the maximum value
+        TreeNode* root = new TreeNode(nums[maxIndex]);
+
+        // Recursively build the left and right subtrees
+        root->left = buildTree(nums, left, maxIndex - 1);
+        root->right = buildTree(nums, maxIndex + 1, right);
+
+        return root;
+    }
+
+    // Display Binary Tree
+    void display(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        cout << root->val << " ";
+        display(root->left);
+        display(root->right);
+    }
+};
+
+// Main function
+int main() {
+    return 0;
+}
+
+
