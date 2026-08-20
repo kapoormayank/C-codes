23f744d27bc9ade69d80608674e91c45cd74fa26
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-20 19:51:38 +0530

    Data Structure And Algorithms

--- DSA/Graphs/UserInput.cpp
+++ DSA/Graphs/UserInput.cpp
@@ -5,6 +5,20 @@
 
 using namespace std;
 
+// Graph Class
+class Graph {
+    // Data Members
+    int V; // Number of vertices
+    list<int>* l;
+
+    // Parameterized Constructor
+    Graph(int v) {
+        this->V = v;
+        l = new list<int>[V];
+    }
+};
+
+// Main function
 int main() {
     return 0;
 }
