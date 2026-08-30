0e93499f429f5b5f5fe2195bf246675285dc5d21
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-30 14:48:52 +0530

    Data Structure And Algorithms

--- DSA/Graphs/Detect_A_Cycle.cpp
+++ DSA/Graphs/Detect_A_Cycle.cpp
@@ -17,13 +17,55 @@ class Graph {
         adj = new list<int>[V];
     }
 
-    // Function to add an edge to the graph
+    // Function to add an edge to the graph undirected
     void addEdge(int u, int v) {
         adj[u].push_back(v); // Add v to u’s list.
         adj[v].push_back(u); // Add u to v’s list (for undirected graph)
     }
 
-    // bool isCyclic();
+    // Function to perform DFS traversal of the graph and detect a cycle
+    bool dfsCyclic(int v,vector<bool>& visited, vector<bool>& recStack) {
+        if(recStack[v]) {
+            return true; // Cycle detected
+        }
+
+        if(visited[v]) {
+            return false; // Already visited
+        }
+
+        visited[v] = true; // Mark the current vertex as visited
+        recStack[v] = true;
+
+        for(int neighbor : adj[v]) {
+            if(dfsCyclic(neighbor,visited,recStack)){
+                return true;
+            }
+        }
+
+        recStack[v] = false;
+        return false;
+    }
+
+    // Function to check if graph contains a cycle
+    bool isCyclic() {
+
+        vector<bool> visited(V, false);
+        vector<bool> recStack(V, false);
+
+        // Check every vertex because graph
+        // can be disconnected
+        for (int i = 0; i < V; i++) {
+
+            if (!visited[i]) {
+
+                if (dfsCyclic(i, visited, recStack)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
     // Print the adjacency list of the graph
     void printAdjList() {
@@ -39,7 +81,7 @@ class Graph {
 
 // Main function
 int main() {
-    
+
     // Create a graph with 5 vertices
     Graph g(5);
 
@@ -49,8 +91,16 @@ int main() {
     g.addEdge(1, 3);
     g.addEdge(2, 4);
 
-    // Print the adjacency list
+    // Print adjacency list
+    cout << "Graph representation:\n";
     g.printAdjList();
+
+    // Check cycle
+    if (g.isCyclic()) {
+        cout << "\nGraph contains a cycle." << endl;
+    } else {
+        cout << "\nGraph does not contain a cycle." << endl;
+    }
 
     return 0;
 }
